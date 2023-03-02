8e1774b9fbfba39d662bbdfd1773de376d0566e6
rodeotr <69229362+rodeotr@users.noreply.github.com>
2023-03-03 00:13:43 +0300

    Searchbox for storage was implemented

--- Views/LeftPanel/TabStorageWordsView.xaml.cs
+++ Views/LeftPanel/TabStorageWordsView.xaml.cs
@@ -1,9 +1,14 @@
 ﻿
+using LungoApp.Windows;
 using LungoApp.Windows.Collections;
+using LungoDatabase.Models;
 using LungoModel.Models;
 using LungoViewModels.ViewModels.Storage;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -26,6 +31,8 @@ namespace LungoApp.Views.LeftPanel
         {
             _vm = (TabStorageWordsViewModel)DataContext;
             _vm.StorageShowtEvent += (context) => ShowContext(context);
+            _vm.WordEditEvent += (member) => ShowWordEdit(member);
+            _vm.AddToCollectionEvent += (member, collections) => AddToCollection(member, collections);
         }
 
         private void ShowContext(StorageContext context)
@@ -33,9 +40,34 @@ namespace LungoApp.Views.LeftPanel
             ShowContextsWindow window = new ShowContextsWindow(context);
             window.Show();
         }
+        private void ShowWordEdit(WordMember member)
+        {
+            EditWordWindow window = new EditWordWindow(member);
+            window.Show();
+        }
+        private void AddToCollection(WordMember member, List<Collection> collections)
+        {
+            AddToCollectionWindow window = new AddToCollectionWindow(member, collections);
+            window.Show();
+        }
 
-       
-    
-    
+        private void SearchChanged(object sender, TextChangedEventArgs args)
+        {
+            string text = ((TextBox)sender).Text;
+            if(text.Length == 0)
+            {
+                _vm.StorageMemberModel.updateCurrentMembers();
+            }
+            else
+            {
+                _vm.CurrentMembers = new ObservableCollection<WordMember>(_vm.StorageMemberModel.AllMembers.Where(a => a.Name.Contains(text)).ToList());
+            }
+            _vm.OnPropertyChanged(nameof(_vm.CurrentMembers));
+
+        } 
+
+
+
+
     }
 }
