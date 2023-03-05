07a88894f59f5a7029e7c8f9bf9a60e4ec6c3c32
rodeotr <69229362+rodeotr@users.noreply.github.com>
2023-03-05 15:43:52 +0300

    media player updated

--- Windows/MediaPlayer.xaml.cs
+++ Windows/MediaPlayer.xaml.cs
@@ -22,15 +22,22 @@ namespace LungoApp.Windows
     public partial class MediaPlayer : Window
     {
         private readonly StorageContext _context;
+        private bool _isPlaying = false;
 
         public MediaPlayer(StorageContext context)
         {
             _context = context;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            Height = screenHeight > 700 ? 700 : screenHeight;
+            Width = screenHeight == 700 ? 1244 : screenHeight * (16/9);
+
             InitializeComponent();
             mediaPlayer.Source = new Uri(((WordContext)(context.Context)).Address.TranscriptionAddress.MediaLocation);
             mediaPlayer.Position = setPosition();
             
             mediaPlayer.Play();
+            _isPlaying = true;
         }
 
         private TimeSpan setPosition()
@@ -41,6 +48,50 @@ namespace LungoApp.Windows
             int seconds = Int32.Parse(position.Substring(6, 2));
             return (TimeSpan.FromHours(hour) + TimeSpan.FromMinutes(minutes) + TimeSpan.FromSeconds(seconds));
         }
+
+        private void PlayOrPause(object sender, EventArgs e)
+        {
+            if (_isPlaying)
+            {
+                mediaPlayer.Pause();
+                playPauseIcon.Kind =  MahApps.Metro.IconPacks.PackIconMaterialKind.Play;
+                _isPlaying = false;
+            }
+            else
+            {
+                mediaPlayer.Play();
+                playPauseIcon.Kind = MahApps.Metro.IconPacks.PackIconMaterialKind.Pause;
+                _isPlaying = true;
+            }
+        }
+        private void Forward10Sec(object sender, EventArgs e)
+        {
+            mediaPlayer.Position += TimeSpan.FromSeconds(10);
+        }
+        private void Reverse10Sec(object sender, EventArgs e)
+        {
+            mediaPlayer.Position -= TimeSpan.FromSeconds(10);
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Space:
+                    PlayOrPause(sender, e);
+                    break;
+                case Key.Escape:
+                    Close();
+                    break;
+                case Key.Right:
+                    Forward10Sec(sender, e);
+                    break;
+                case Key.Left:
+                    Reverse10Sec(sender, e);
+                    break;
+            }
+        }
+
         private void Window_Closed(object sender, EventArgs e)
         {
             mediaPlayer.Stop();
